b2faaa5543ad0e87532d2f9a6ab8021cec25f7a6
sdemarco <sdemarco@pagustech.com>
2020-06-03 00:33:33 -0300

    Prototipo Inicial

---
+++ app/src/main/java/ar/com/tallerdeprogramacion/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package ar.com.tallerdeprogramacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
---
+++ app/src/main/java/ar/com/tallerdeprogramacion/MainActivity.kt
@@ -0,0 +1,20 @@
+package ar.com.tallerdeprogramacion
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.search_button)
+        button.setOnClickListener{
+            val intent = Intent(this, DetailProductActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
