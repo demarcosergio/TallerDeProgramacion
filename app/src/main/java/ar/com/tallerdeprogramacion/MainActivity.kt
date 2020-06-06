2df6622462c6e4795251df1f60b478378e5a74bd
sdemarco <sdemarco@pagustech.com>
2020-06-06 01:03:00 -0300

    ids modificados y transicion agregada

--- app/src/main/java/ar/com/tallerdeprogramacion/MainActivity.kt
+++ app/src/main/java/ar/com/tallerdeprogramacion/MainActivity.kt
@@ -1,20 +1,32 @@
 package ar.com.tallerdeprogramacion
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
+import kotlinx.android.synthetic.main.activity_detail_product.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imageView = findViewById<View>(R.id.imageViewLogo) as ImageView
         val button = findViewById<Button>(R.id.search_button)
         button.setOnClickListener{
             val intent = Intent(this, DetailProductActivity::class.java)
-            startActivity(intent)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this,imageView,
+                ViewCompat.getTransitionName(imageView).toString()
+            )
+            startActivity(intent,options.toBundle())
+            //startActivity(intent)
         }
     }
 }
